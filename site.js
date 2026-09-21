25c7e0ba7d09091103c7dc520e8463fb7f31edf2
lukiz <apple@macbook-air-lukiz.home>
2026-09-21 15:23:26 +0200

    Liczby: jedna proporcja 3x w całym serwisie, link do kalendarza

--- site.js
+++ site.js
@@ -1,4 +1,4 @@
-const BOOKING_URL = "BOOKING_URL_DO_UZUPELNIENIA";
+const BOOKING_URL = "https://calendar.app.google/GWuQbhH2p4hGBfy58";
 
 document.addEventListener('DOMContentLoaded', function(){
   document.querySelectorAll('[data-booking-url]').forEach(function(link){
